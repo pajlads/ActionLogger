a80b63e68d98653d92e41c4fa74fad3215b488fe
iProdigy <gitprodigy@proton.me>
2024-08-24 13:55:15 -0700

    refactor: file writing

--- src/main/java/actionlogger/writers/JsonWriter.java
+++ src/main/java/actionlogger/writers/JsonWriter.java
@@ -2,36 +2,61 @@ package actionlogger.writers;
 
 import actionlogger.Utils;
 import com.google.gson.Gson;
-import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
+import net.runelite.client.RuneLite;
 
 import javax.annotation.Nonnull;
-import java.io.FileOutputStream;
+import java.io.BufferedWriter;
+import java.io.Closeable;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.concurrent.ExecutorService;
 
 @Slf4j
-@RequiredArgsConstructor
-public class JsonWriter {
+public class JsonWriter implements Closeable {
     private final Gson gson;
     private final Client client;
-    private final FileOutputStream fh;
+    private final ExecutorService executor;
+    private final BufferedWriter fh;
 
-    public void write(@Nonnull String type, @Nonnull Object data) {
-        try {
-            var payload = new Payload(this.client.getTickCount(), Utils.getTimestamp(), type, data);
-            fh.write(gson.toJson(payload).getBytes());
-            fh.write('\n');
-        } catch (IOException e) {
-            log.warn("Failed to write DialogueTracker data: {}", e.getMessage());
-        }
+    public JsonWriter(Gson gson, Client client, ExecutorService executor) throws IOException {
+        this.gson = gson;
+        this.client = client;
+        this.executor = executor;
+
+        var dir = new File(RuneLite.RUNELITE_DIR, "action-logger");
+        //noinspection ResultOfMethodCallIgnored
+        dir.mkdir();
+
+        var path = dir.toPath().resolve(String.format("%d-logs.txt", System.currentTimeMillis()));
+        this.fh = Files.newBufferedWriter(path);
     }
 
-    @RequiredArgsConstructor
-    static private class Payload {
-        private final Integer tickCount;
-        private final String timestamp;
-        private final String type;
-        private final Object data;
+    public void write(@Nonnull String type, @Nonnull Object data) {
+        var payload = new Payload(this.client.getTickCount(), Utils.getTimestamp(), type, data);
+        executor.execute(() -> {
+            try {
+                fh.write(gson.toJson(payload));
+                fh.newLine();
+            } catch (IOException e) {
+                log.warn("Failed to write DialogueTracker data: {}", e.getMessage());
+            }
+        });
+    }
+
+    @Override
+    public void close() throws IOException {
+        fh.close();
+    }
+
+    @Value
+    private static class Payload {
+        int tickCount;
+        String timestamp;
+        String type;
+        Object data;
     }
 }
