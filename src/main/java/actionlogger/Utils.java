0ea1d41b55ac045f1fd18555f23e7375f2452676
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2024-08-25 11:01:32 +0200

    use utc timestamps in log file

--- src/main/java/actionlogger/Utils.java
+++ src/main/java/actionlogger/Utils.java
@@ -1,14 +1,11 @@
 package actionlogger;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 public class Utils {
 
-    private final static DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
-
     public static String getTimestamp() {
-        var now = LocalDateTime.now();
-        return now.format(DATE_TIME_FORMATTER);
+        return Instant.now().truncatedTo(ChronoUnit.SECONDS).toString();
     }
 }
