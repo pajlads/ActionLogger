ab11ebdff5a1dad0c996d983c0a263d8ad37dee7
iProdigy <8106344+iProdigy@users.noreply.github.com>
2025-07-19 05:58:10 -0500

    refactor: improve thread safety of JsonWriter (#45)

--- src/main/java/actionlogger/writers/JsonWriter.java
+++ src/main/java/actionlogger/writers/JsonWriter.java
@@ -2,20 +2,21 @@ package actionlogger.writers;
 
 import actionlogger.Utils;
 import com.google.gson.Gson;
-import lombok.Getter;
 import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.client.RuneLite;
+import org.apache.commons.lang3.tuple.Pair;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.io.BufferedWriter;
 import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 
 @Slf4j
@@ -24,9 +25,8 @@ public class JsonWriter implements Closeable {
     private final Client client;
     private final ExecutorService executor;
     private final File dir;
-    @Getter
-    private @Nullable Path path = null;
-    private @Nullable BufferedWriter fh = null;
+    private Path path = null;
+    private BufferedWriter fh = null;
 
     public JsonWriter(Gson gson, Client client, ExecutorService executor) {
         this.gson = gson;
@@ -40,52 +40,66 @@ public class JsonWriter implements Closeable {
         this.restartFile();
     }
 
-    public void restartFile() {
-        if (this.fh != null) {
-            this.close();
-        }
+    public CompletableFuture<Map.Entry<Path, Path>> restartFile() {
+        CompletableFuture<Map.Entry<Path, Path>> future = new CompletableFuture<>();
+        executor.execute(() -> {
+            var oldFh = this.fh;
+            var oldPath = this.path;
+            var path = dir.toPath().resolve(String.format("%d-logs.txt", System.currentTimeMillis()));
+            try {
+                this.fh = Files.newBufferedWriter(path);
+                this.path = path;
 
-        this.path = dir.toPath().resolve(String.format("%d-logs.txt", System.currentTimeMillis()));
-        try {
-            this.fh = Files.newBufferedWriter(this.path);
-        } catch (IOException e) {
-            log.warn("Could not create file at {}", this.path);
-        }
+                if (oldFh != null) {
+                    try {
+                        oldFh.close();
+                    } catch (IOException e) {
+                        log.warn("Failed to close old writer for {}", oldPath, e);
+                    }
+                }
+
+                future.complete(Pair.of(oldPath, path));
+            } catch (IOException e) {
+                log.warn("Could not create file at {}", path, e);
+                future.completeExceptionally(e);
+            }
+        });
+        return future;
     }
 
     public void write(@Nonnull String type, @Nonnull Object data) {
-        if (this.fh == null) {
-            return;
-        }
         var payload = new Payload(this.client.getTickCount(), Utils.getTimestamp(), type, data);
-        var currentFh = this.fh;
         executor.execute(() -> {
+            var currentFh = this.fh;
+            if (currentFh == null) {
+                log.debug("Skipping write due to closed resource: {}", payload);
+                return;
+            }
             try {
                 currentFh.write(gson.toJson(payload));
                 currentFh.newLine();
                 currentFh.flush();
             } catch (IOException e) {
-                log.warn("Failed to write ActionLogger data: {}", e.getMessage());
+                log.warn("Failed to write ActionLogger data", e);
             }
         });
     }
 
     @Override
     public void close() {
-        if (this.fh == null) {
-            return;
-        }
-
-        var currentFh = this.fh;
-        var currentPath = this.path;
-
-        this.fh = null;
-
         executor.execute(() -> {
+            var currentFh = this.fh;
+            if (currentFh == null) {
+                return;
+            }
+            var currentPath = this.path;
+
             try {
                 currentFh.close();
+                this.fh = null;
+                this.path = null;
             } catch (IOException e) {
-                log.warn("Failed to close file at {}", currentPath);
+                log.warn("Failed to close file at {}", currentPath, e);
             }
         });
     }
