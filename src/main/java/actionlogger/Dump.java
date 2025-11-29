2e6181467360f82b6057e08067553aa3d93e4e9f
pajlada <rasmus.karlsson@pajlada.com>
2025-11-29 11:38:04 +0100

    feat: dump widgets (#58)
    Co-authored-by: iProdigy <gitprodigy@proton.me>

--- src/main/java/actionlogger/Dump.java
+++ src/main/java/actionlogger/Dump.java
@@ -2,22 +2,31 @@ package actionlogger;
 
 import actionlogger.writers.JsonWriter;
 import lombok.Data;
+import lombok.Value;
 import net.runelite.api.Client;
+import net.runelite.api.widgets.Widget;
 import net.runelite.client.game.ItemManager;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.awt.Rectangle;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Dump {
     public static void handleDump(@Nonnull ActionLoggerPlugin plugin, @Nonnull Client client, @Nonnull JsonWriter writer, @Nonnull ItemManager itemManager, @Nonnull String[] args) {
+        assert (client.isClientThread());
+
         var dumps = new HashSet<String>();
         // default dump everything
         dumps.add("grounditems");
         dumps.add("objects");
         dumps.add("npcs");
+        dumps.add("widgets");
 
         if (args.length > 1) {
             dumps.clear();
@@ -95,6 +104,13 @@ public class Dump {
             npcData.add(new NPCData(localLocation.getSceneX(), localLocation.getSceneY(), worldView.getPlane(), npc.getId(), npc.getName()));
         }
 
+        var widgets = new ArrayList<WidgetData>();
+
+        for (var widgetRoot : client.getWidgetRoots()) {
+            var widgetData = WidgetData.from(widgetRoot);
+            widgets.add(widgetData);
+        }
+
         var dumpData = new DumpData(
             sceneData,
             worldView.getPlane(),
@@ -103,7 +119,8 @@ public class Dump {
             dumps.contains("objects") ? wallObjectData : null,
             dumps.contains("objects") ? gameObjectData : null,
             dumps.contains("objects") ? groundObjectData : null,
-            dumps.contains("npcs") ? npcData : null
+            dumps.contains("npcs") ? npcData : null,
+            dumps.contains("widgets") ? widgets : null
         );
 
         writer.write("DUMP", dumpData);
@@ -147,6 +164,48 @@ public class Dump {
         private final String name;
     }
 
+    @Value
+    private static class WidgetData {
+        int id;
+        int type;
+        int contentType;
+        int parentId;
+        String text;
+        String name;
+        Integer itemID;
+        Integer modelID;
+        Integer spriteID;
+        Rectangle bounds;
+        boolean hidden;
+        List<WidgetData> dynamicChildren;
+        List<WidgetData> staticChildren;
+        List<WidgetData> nestedChildren;
+
+        public static WidgetData from(@Nonnull Widget w) {
+            var bounds = w.getBounds();
+            boolean emptyBounds = bounds.getWidth() == 0 && bounds.getHeight() == 0 && bounds.getX() == -1 && bounds.getY() == -1;
+            var dynamicChildren = Arrays.stream(w.getDynamicChildren()).filter(Objects::nonNull).map(WidgetData::from).collect(Collectors.toList());
+            var staticChildren = Arrays.stream(w.getStaticChildren()).filter(Objects::nonNull).map(WidgetData::from).collect(Collectors.toList());
+            var nestedChildren = Arrays.stream(w.getNestedChildren()).filter(Objects::nonNull).map(WidgetData::from).collect(Collectors.toList());
+            return new WidgetData(
+                w.getId(),
+                w.getType(),
+                w.getContentType(),
+                w.getParentId(),
+                w.getText() == null || w.getText().isEmpty() ? null : w.getText(),
+                w.getName() == null || w.getName().isEmpty() ? null : w.getName(),
+                w.getItemId() >= 0 ? w.getItemId() : null,
+                w.getModelId() >= 0 ? w.getModelId() : null,
+                w.getSpriteId() >= 0 ? w.getSpriteId() : null,
+                emptyBounds ? null : w.getBounds(),
+                w.isHidden(),
+                dynamicChildren.isEmpty() ? null : dynamicChildren,
+                staticChildren.isEmpty() ? null : staticChildren,
+                nestedChildren.isEmpty() ? null : nestedChildren
+            );
+        }
+    }
+
     @Data
     private static class DumpData {
         private final SceneData scene;
@@ -158,5 +217,6 @@ public class Dump {
         private final @Nullable List<ObjectData> gameObjects;
         private final @Nullable List<ObjectData> groundObjects;
         private final @Nullable List<NPCData> npcs;
+        private final @Nullable List<WidgetData> widgets;
     }
 }
